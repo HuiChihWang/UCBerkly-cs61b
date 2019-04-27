fec7a79c45523a8f2ffa28144317d75fc48df9a7
HuiChihWang <taya87136@gmail.com>
2019-04-28 00:46:14 +0800

    finish Node and BSTMap

--- lab7/BSTMap.java
+++ lab7/BSTMap.java
@@ -1,2 +1,96 @@
-public class BSTMap {
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<KeyType extends Comparable<KeyType>, ValueType> implements Map61B<KeyType, ValueType> {
+    private Node<KeyType, ValueType> BSTRoot;
+    private int numNodes;
+
+    public BSTMap(){
+        clear();
+    }
+
+    public void clear(){
+        numNodes = 0;
+        BSTRoot = null;
+    }
+
+    public boolean containsKey(KeyType key){
+        return GetRecursive(BSTRoot, key) != null;
+    }
+
+    public ValueType get(KeyType key){
+        Node<KeyType, ValueType> find_node = GetRecursive(BSTRoot, key);
+        if(find_node == null)
+            return null;
+
+        return find_node.GetValue();
+    }
+
+    public int size(){
+        return numNodes;
+    }
+
+    public void put(KeyType key, ValueType value){
+        BSTRoot = PutRecursive(BSTRoot, key, value);
+        numNodes += 1;
+    }
+
+
+    public Set<KeyType> keySet(){
+        throw new UnsupportedOperationException();
+    }
+
+    public ValueType remove(KeyType key, ValueType value){
+        throw new UnsupportedOperationException();
+    }
+
+    public ValueType remove(KeyType key){
+        throw new UnsupportedOperationException();
+    }
+
+    public Iterator<KeyType> iterator(){
+        throw new UnsupportedOperationException();
+    }
+
+    public void printInOrder(){
+        printInOrderRecursive(BSTRoot);
+    }
+
+    private Node<KeyType, ValueType> CreateNode(KeyType key, ValueType value){
+        return Node.CreateNode(key, value);
+    }
+
+    private Node<KeyType, ValueType> PutRecursive(Node<KeyType, ValueType> root, KeyType key, ValueType value){
+        if(root == null)
+            return CreateNode(key, value);
+
+        if(key.compareTo(root.GetKey()) < 0)
+            root.SetLeftNode(PutRecursive(root.GetLeftNode(), key, value));
+        else  if(key.compareTo(root.GetKey()) > 0)
+            root.SetRightNode(PutRecursive(root.GetRightNode(), key, value));
+
+        return root;
+    }
+
+    private Node<KeyType, ValueType> GetRecursive(Node<KeyType, ValueType> root, KeyType key){
+        if(root == null)
+            return null;
+
+        if(key.compareTo(root.GetKey()) < 0)
+            return GetRecursive(root.GetLeftNode(), key);
+        else if(key.compareTo(root.GetKey()) > 0)
+            return GetRecursive(root.GetRightNode(), key);
+
+        else
+            return root;
+    }
+
+    private void printInOrderRecursive(Node<KeyType, ValueType> root){
+        if(root == null)
+            return;
+
+        printInOrderRecursive(root.GetLeftNode());
+        System.out.println(root);
+        printInOrderRecursive(root.GetRightNode());
+    }
 }
--- lab7/Node.java
+++ lab7/Node.java
@@ -1,2 +1,41 @@
-public class Node {
+public class Node<KeyType, ValueType> {
+    private KeyType key;
+    private ValueType value;
+    private Node<KeyType, ValueType> left;
+    private Node<KeyType, ValueType> right;
+
+
+    public static <KeyType,ValueType>  Node<KeyType,ValueType> CreateNode(KeyType key, ValueType value){
+        return new Node<>(key, value);
+    }
+
+    public Node(KeyType key, ValueType value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public void SetLeftNode(Node<KeyType, ValueType> node){
+        left = node;
+    }
+
+    public void SetRightNode(Node<KeyType, ValueType> node){
+        right = node;
+    }
+
+    public Node GetLeftNode(){
+        return left;
+    }
+
+    public Node GetRightNode(){
+        return right;
+    }
+
+    public KeyType GetKey(){
+        return key;
+    }
+
+    public ValueType GetValue(){
+        return value;
+    }
+
 }
