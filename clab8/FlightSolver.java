95dfeb80facbf186aa8cba824092c79976724aab
HuiChihWang <taya87136@gmail.com>
2019-05-01 18:45:19 +0800

    comment error

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
- * Assumes valid input, i.e. all Flight start times are >= end times.
+ * Assumes valid input, i.e. all Flight start times are <= end times.
  * If a flight starts at the same time as a flight's end time, they are
  * considered to be in the air at the same time.
  */
